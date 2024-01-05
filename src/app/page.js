ec8cef848fc2c023215fbce5e73ea8d7ad7b38e8
amiribnjaman <amiribnjaman@gmail.com>
2024-01-05 20:59:08 +0600

    Banner section implemented

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
 import Image from 'next/image'
+import bannerImg from '@/Assets/Image1.png'
 
 export default function Home() {
   return (
     <main className="">
-      
+      {/*------------HOME PAGE BANNER SECTION------------ */}
+      <section className="w-[75%] mx-auto my-24 bg-[#F99F1C] rounded-lg h-auto flex items-center px-8 text-white">
+        <div className="pl-16">
+          <h1 className="text-4xl font-semibold tracking-wider leading-tight text-[#FFEFE7]">
+            Deliver Food To Your Door Step!
+          </h1>
+          <p className="text-[#ECD0A8] mt-3">
+            Authentic food, Quick Service, Fast Delivery
+          </p>
+        </div>
+        <div>
+          <Image src={bannerImg} alt="banner-img" />
+        </div>
+      </section>
     </main>
-  )
+  );
 }
